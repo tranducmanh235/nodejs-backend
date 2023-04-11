fe8c0352b83200ffbb50212a3bdea5f14b762bbc
Tran Duc Manh <tranducmanh235@gmail.com>
2023-04-11 12:49:28 +0700

    using template engines ejs

--- src/server.js
+++ src/server.js
@@ -1,18 +1,20 @@
 // import express from "express";
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const port = 8081;
+
+// config template engine
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // khai bao route
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 app.get("/hello", (req, res) => {
-    res.send("Hello the second time!");
-});
-app.get("/kaka", (req, res) => {
-    res.send("<h1>Kaka</h1>");
+    res.render("sample.ejs");
 });
 
 app.listen(port, () => {
